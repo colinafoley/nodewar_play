42fbd593bc62b44f661dcaa69c52bc4daf67ec7b
Colin A Foley <colin.a.foley@gmail.com>
2013-04-12 13:03:51 -0400

    Reported queen position to mothership in queenStep.

--- obsidian_rocks.js
+++ obsidian_rocks.js
@@ -12,43 +12,12 @@ ai.step = function(o) {
   }
 };
 
-function queenStep(o) {
-  return {
-    torque: 0,
-    thrust: 0,
-  }
-}
-function droneStep(o) {
-  var md, spd, thrust, torque;
-  torque = 0;
-  thrust = 0;
-  spd = speed_to_nearest_moon(o);
-  md = dist_to_nearest_moon(o);
-  if ((spd > 0.1) && (md < 0.8 * o.game.moon_field)) {
-    thrust = 1.0;
-  }
-  if (o.me.queen) {
-      thrust = 0;
-  }
-  return {
-    torque: torque,
-    thrust: thrust
-  };
-}
-
-speed_to_nearest_moon = function(o) {
-  return o.lib.physics.speedToward(o.me.vel, o.me.pos, o.moons[0].pos);
-};
-
-dist_to_nearest_moon = function(o) {
-  return o.moons[0].dist;
-};
-
 var my_start_pos = null,
   speed_change_fear = 50,
   threat_thresh = 0.5;
 
 function queenStep (o) {
+  report_queen_position(o);
   var threats = get_threats(o),
     new_target = find_ideal_position(o, threats),
     _ref = o.lib.targeting.simpleTarget(o.me, new_target),
@@ -59,6 +28,10 @@ function queenStep (o) {
     thrust: thrust,
     label: ~~threats[0].threat_factor
   };
+}
+
+function report_queen_position(o) {
+  o.mothership.queen_position = o.me.pos;
 }
 
 function get_threats (o) {
@@ -165,3 +138,30 @@ function get_board_exit_pos(o) {
   }
   return [px, py];
 }
+
+function droneStep(o) {
+  var md, spd, thrust, torque;
+  torque = 0;
+  thrust = 0;
+  spd = speed_to_nearest_moon(o);
+  md = dist_to_nearest_moon(o);
+  if ((spd > 0.1) && (md < 0.8 * o.game.moon_field)) {
+    thrust = 1.0;
+  }
+  if (o.me.queen) {
+      thrust = 0;
+  }
+  return {
+    torque: torque,
+    thrust: thrust
+  };
+}
+
+speed_to_nearest_moon = function(o) {
+  return o.lib.physics.speedToward(o.me.vel, o.me.pos, o.moons[0].pos);
+};
+
+dist_to_nearest_moon = function(o) {
+  return o.moons[0].dist;
+};
+
